f1cbe9a9439ea5e751ede085f14c6f6f16646473
mac <20j.code@gmail.com>
2024-05-14 14:30:44 +0900

    feat(UserGameData) : add struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,30 @@
-use near_sdk::store::unordered_map;
+use near_sdk::collections::UnorderedMap; //5.1.0 ver
 use near_sdk::{log, near, near_bindgen};
 use near_sdk::borsh::{self, BorshDeserialize,BorshSerialize};
 
 #[near_bindgen] //니어 초기화 매크로?
-#[derive(BorshDeserialize, BorshSerialize,Default)] //직렬화
-pub struct NearPower {
-    near_power : unordered_map<String,i16>,
-    my_money : i16,
+#[derive(BorshDeserialize, BorshSerialize)] //직렬화
+pub struct NearPowerGame {
+    user_lists: UnorderedMap<String,UserGameData>
+}
+
+impl Default for NearPowerGame {
+    fn default() -> Self {
+        Self {
+            user_lists : UnorderedMap::new(b"user_lists".to_vec()),
+        }
+    }
 }
 
 #[near_bindgen]
-impl NearPower {
-    #[init]
-    pub fn new() -> Self {
-        Self {
-            near_power : 0,
-            my_money : 0,
-        }
-    }
-    pub fn power_up(&mut self) -> i16 {
-        if self.my_money>0 {
-            self.my_money : self.my_money -1;
-            self.near_power: self.near_power + 1;
-        }
-        return self.near_power;
-    }
-    pub fn mining(&mut self) -> i16 {
-        self.my_money : self.my_money + 1;
-        return self.my_money;
-    }
+impl NearPowerGame {
+
+}
+
+#[derive( BorshSerialize,BorshDeserialize)]
+pub struct UserGameData {
+    id : String,
+    near_power : i32,
+    near_money : i32,
+    owner : String,
 }
