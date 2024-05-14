e1561a761b78d2e60659b066b860d1992cfba881
mac <20j.code@gmail.com>
2024-05-14 15:10:34 +0900

    feat(UserGameData::new_user) : add new_user method

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use near_sdk::collections::UnorderedMap; //5.1.0 ver
-use near_sdk::{log, near, near_bindgen};
+use near_sdk::{AccountId, env, log, near, near_bindgen};
 use near_sdk::borsh::{self, BorshDeserialize,BorshSerialize};
 
 #[near_bindgen] //니어 초기화 매크로?
 #[derive(BorshDeserialize, BorshSerialize)] //직렬화
 pub struct NearPowerGame {
-    user_lists: UnorderedMap<String,UserGameData>
+    user_lists: UnorderedMap<String,UserGameData>,
+    user_count: i16
 }
 
 impl Default for NearPowerGame {
     fn default() -> Self {
         Self {
             user_lists : UnorderedMap::new(b"user_lists".to_vec()),
+            user_count : 0
         }
     }
 }
@@ -23,8 +25,21 @@ impl NearPowerGame {
 
 #[derive( BorshSerialize,BorshDeserialize)]
 pub struct UserGameData {
-    id : String,
+    id : i16,
     near_power : i32,
     near_money : i32,
-    owner : String,
+    owner : AccountId, //signer의 아이디를 받는다.
+}
+
+impl UserGameData {
+    pub fn new_user(&self,user_count : i16,
+                    user_base_money : i32,
+                    user_base_power : i32)->Self {
+        Self {
+            id : user_count,
+            near_power : user_base_power,
+            near_money : user_base_money,
+            owner : env::signer_account_id()
+        }
+    }
 }
